6770806183ef99c4fbce27b906a30ba9ad19bbf2
Marcos Acosta <marcos@acosta.tv>
2012-04-02 08:45:29 -0500

    remove gui settings

--- init.el
+++ init.el
@@ -1,15 +1,9 @@
 (setq-default inhibit-startup-message t)
 
 ;; appearance
-(tool-bar-mode 0)
 (menu-bar-mode 0)
-(scroll-bar-mode 0)
 (global-linum-mode 1)
 (show-paren-mode t)
-(setq-default cursor-type '(bar . 1))
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
-(setq scroll-step 1)
-(set-frame-font "Menlo-13")
 
 ;; disable backup
 (setq backup-inhibited t)
