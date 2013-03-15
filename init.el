96765ceeeb1b81ccb0d11096b545ebf1922640c6
Marcos Acosta <marcos@acosta.tv>
2013-03-15 08:19:36 -0500

    appearance tweaks

--- init.el
+++ init.el
@@ -1,9 +1,17 @@
-(setq-default inhibit-startup-message t)
+(push "/usr/local/bin" exec-path)
 
 ;; appearance
+(setq-default inhibit-startup-message t)
 (menu-bar-mode 0)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 (global-linum-mode 1)
 (show-paren-mode t)
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
+(set-frame-font "Menlo-13")
+(load-theme 'tango)
 
 ;; disable backup
-(setq backup-inhibited t)
+(setq make-backup-files nil)
+(setq auto-save-default nil)
